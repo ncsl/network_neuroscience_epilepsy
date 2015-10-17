994c85937ecfe22cd62ba60dcd39aa03bb694f14
Bobby Norton <bobby@testedminds.com>
2015-10-16 20:55:38 -0500

    Refactor power_coherence for clarity

--- tools/eeg2fsv/eeg2fsv.m
+++ tools/eeg2fsv/eeg2fsv.m
@@ -1,42 +1,61 @@
-% Inputs ----------------------------------------------------------------------
-
-fs = 1000;
-pos = 0;
-
 home = getenv('HOME');
-eegHome = '/dev/eztrack/tools/output/eeg/';
-patient = 'PY12N008';
-patientFile = 'PY12N008_07_21_2012_14-05-48_640sec';
-pathval = [home eegHome patient];
+eeg_output = '/dev/eztrack/tools/output/eeg';
+
+% TODO: Inputs to fn-----------------------------------------------------------------
+% working_directory
+% patient_id
+% frequency
+% num_channels
+% sample_to_access
+
+working_directory = [home eeg_output];
+patient_id = 'PY12N008';
+frequency = 1000;
 
 % nChannels depends on the patient - this will need to be an input parameter: 
 % length(channels)?
 % some of the hard-coded channels don't match this in the original impl.
-
 % There are reference channels that are removed later in the process.
 % Some channels are also removed from the analysis if they are consistently 
 % noisy or don't change during a seizure.
 % TODO: Try leaving in all channels to compare the results.
-
 % NB: Cleveland is more complicated because some channels are in white matter,
 % and should be excluded.
-nChannels = 89;
+num_channels = 89;
+
+sample_to_access = 0;
+
+%----------------------------------------------------------------------
+
+patient_file_path = [working_directory '/' patient_id '/'];
+
+% Find mef2eeg/edf2eeg output files in '.../PY12N008/PY12N008_07_21_2012_14-05-48_640sec.mat'...
+patient_files = dir([patient_file_path patient_id '*.mat']);
+patient_file_names = {patient_files.name};
 
 %------------------------------------------------------------------------------
-% 1. Compute Adjacency matrix sequence
-power_coherence(pathval, patientFile, nChannels, fs, pos)
+% 1. Compute adjacency matrix sequence
+% - matrices are stored into 1-D arrays
+parpool;
+parfor i = 1:length(patient_file_names)
+    power_coherence(patient_file_path, patient_file_names{i}, num_channels, frequency, sample_to_access);
+end
+delete(gcp('nocreate'));
 
-%               - matrices are stored into 1-D arrays
 %------------------------------------------------------------------------------
 
 % 2. Compute ranked-Eigenvector Centrality (rEVC) sequence
-% TODO: If we're going to filter channels, this fn takes a parameter to do so.
-svd_decomposition([pathval '/adj_pwr'], patientFile, nChannels)
-svdVectorPath = sprintf('%s%s%s/adj_pwr/svd_vectors', home, eegHome, patient);
-svdVectorFile = 'svd_l_pwr_PY12N008_07_21_2012_14-05-48_640sec_gamma.dat';
-extraction_vectors(svdVectorPath, svdVectorFile, nChannels)
+% TODO: If we're going to filter channels, this fn takes a parameter 'included_chn' to do so.
+% svd_decomposition([pathval '/adj_pwr'], patientFile, nChannels);
 
-% Quick test: Check that all signs are the same in each column
+
+% svdVectorPath = sprintf('%s%s%s/adj_pwr/svd_vectors', home, eegHome, patient);
+% svdVectorFile = 'svd_l_pwr_PY12N008_07_21_2012_14-05-48_640sec_gamma.dat';
+
+% extraction_vectors(svdVectorPath, svdVectorFile, nChannels);
+
+
+% TODO: Quick test: Check that all signs are the same in each column
 % There are also cases of extremely small values that should be zero.
 
 %------------------------------------------------------------------------------
--- tools/eeg2fsv/power_coherence.m
+++ tools/eeg2fsv/power_coherence.m
@@ -1,27 +1,19 @@
-function power_coherence(pathval,filename,num_channels,Fs,pos)
+function power_coherence(pathval, filename, num_channels, fs, sample_to_access)
 
-% Description: This function computes (i) the auto- and cross-power, (ii)
-%              and the mean phase coherence among iEEG recordings from
-%              independent channels. Power and coherence are evaluated in
+% Description: This function computes the cross-power among iEEG recordings from
+%              independent channels. Power spectra are evaluated in
 %              specific frequency bands. The iEEG recordings are extracted
 %              from a *.mat file whose name and home directory are provided
 %              along with the data format. A pointer to the first sample to
 %              be accessed and the # of independent channels to be used are
-%              also provided. Frequency bands of interest are:
-%               - delta: [ 0,  4) Hz;
-%               - theta: [ 4,  8) Hz;
-%               - alpha: [ 8, 13) Hz;
-%               - beta:  [13, 30) Hz;
-%               - gamma: [30, 90) Hz.
+%              also provided.
 %
-%             The sampling rate of the input is specified by the user.
-%             Spectra and coherence values are computed over a sliding
-%             window (2.5s-long, 1.5s-long overlap) with the Welch's method
-%             i.e., each  window of data is divided into 1s-long sub-
-%             sections (750ms overlap) and the power (coherence) is
-%             averaged across the sub-sections. Power spectra and mean
-%             coherence values fill band-specific connectivity matrices,
-%             which are stored in *.bin file.
+%              The sampling rate of the input is specified by the user.
+%              Spectra are computed over a sliding window (2.5s-long, 1.5s-long overlap) 
+%              with Welch's method i.e., each  window of data is divided into 1s-long 
+%              sub-sections (750ms overlap) and the power is averaged across the sub-sections. 
+%              The frequency fs is used as the number of points of the fft.
+%              Power spectra fill band-specific connectivity matrices, which are stored in *.dat file.
 %             
 % Input:    pathval      - Path to the directory where the destination and
 %                          source files are stored. It must be a string of
@@ -34,30 +26,25 @@ function power_coherence(pathval,filename,num_channels,Fs,pos)
 %           num_channels - Number of independent iEEG channels to be used.
 %                          It must be a positive integer.
 %
-%           Fs           - Sampling frequency (in Hz). It must be positive.
+%           fs           - Sampling frequency (in Hz). It must be positive.
 %
-%           pos          - pointer to the last second of data accessed
-%                          (counted from 0), i.e., the first byte to-be-
-%                          extracted is in position pos x num_channels x
-%                          #-samples-per-second x #-bytes-per-sample + 1.
-%                          It must be a nonnegative integer.
+%           sample_to_access  - pointer to the last second of data accessed
+%                             (counted from 0), i.e., the first byte to-be-
+%                             extracted is in position pos x num_channels x
+%                             #-samples-per-second x #-bytes-per-sample + 1.
+%                             It must be a nonnegative integer.
 %
 % Output:   no output returned.
 %
-%
-%
-% NOTE: Power spectra and coherence values estimated for a given window of
-%       data in a given frequency band are stored in a 1-D array. Denoted
-%       with 1... n the iEEG channels, the cross-power (coherence) between
-%       each pair of channels is stored according to the following rule:
+% NOTE: Power spectra estimated for a given window of data in a given frequency 
+%       band are stored in a 1-D array. Denoted with 1... n the iEEG channels, 
+%       the cross-power between each pair of channels is stored according to the 
+%       following rule:
 %
 %        - array position: 1  2...  n n+1 n+2  2n-1 2n ... 3n-2... n(n+1)/2
 %        - channel pair:  11 22... nn  12  13... 1n 23 ...   2n...   (n-1)n
 %
-%       Values estimated for consecutive windows are added to a 2-D array
-%       by row.
-%			
-%
+%       Values estimated for consecutive windows are added to a 2-D array by row.
 %
 % Author: S. Santaniello
 % Modified by: B. Chennuri
@@ -69,36 +56,51 @@ function power_coherence(pathval,filename,num_channels,Fs,pos)
 %--------------------------------------------------------------------------
 % validate inputs
 %--------------------------------------------------------------------------
-if (nargin<5)
-    error('Error: no enough input')
+if (nargin < 5)
+    error('Error: no enough input');
 end
 
 if (isempty(pathval) || ~ischar(pathval) || ~isdir(pathval))
     error('Error: path is not a valid directory');
 end
 
-if (isempty(filename) || ~ischar(filename) || ~exist(sprintf('%s/%s.mat',pathval,filename),'file'))
-    error('Error: name of the source file not valid or file not found');
+eeg_filename = sprintf('%s/%s', pathval, filename);
+if (isempty(filename) || ~ischar(filename) || ~exist(eeg_filename,'file'))
+    error('Error: Could not open %s', eeg_filename);
 end
 
 % check if the file is corrupted and extract the length of the file (in number of bytes)
-fid = load(sprintf('%s/%s.mat',pathval,filename));
-lengthfile = length(fid.eeg);
-if (lengthfile == 0)
-    clear fid
-    error('Error: file not open correctly'); 
+eeg_file = load(eeg_filename);
+file_length = length(eeg_file.eeg);
+if (file_length == 0)
+    error('Error: File missing eeg data'); 
 end
+
+% TODO: Remove the file if it exists.
+if ~exist(sprintf('%s/adj_pwr', pathval), 'dir')
+    mkdir(sprintf('%s', pathval), 'adj_pwr');
+end
+
+[~,basename,~] = fileparts(eeg_filename);
+
+output_file = fopen(sprintf('%s/adj_pwr/adj_pwr_%s_gamma.dat', pathval, basename), 'wb');
+
+% frequency bands (in Hz) used during the computation of the connectivity matrices
+% conventional frequencies corresponding to the bands below:
+% {'delta';'theta';'alpha';'beta';'gamma'};
+bands = [0 4; 4 8; 8 13; 13 30; 30 90];
+% EZTrack only uses the gamma band
+gamma = bands(5,:);
 
 if (isempty(num_channels) || ~isreal(num_channels) || length(num_channels)>1 || num_channels<1)
     error('Error: Invalid number of recording channels');
 end
 num_channels = round(num_channels);
 
-if (isempty(Fs) || ~isreal(Fs) || length(Fs)>1 || Fs<1)
+if (isempty(fs) || ~isreal(fs) || length(fs)>1 || fs<1)
     error('Error: sampling frequency not valid');
 end
-Fs = round(Fs);
-
+fs = round(fs);
 
 %--------------------------------------------------------------------------
 
@@ -109,74 +111,43 @@ Fs = round(Fs);
 % notch filter (stop frequency: 60Hz; stop-band: 4Hz)
 % represented by a transfer function: polynomial / polynomial
 % continuous second-order equations, in this case.
-if (Fs==1000)
+if (fs==1000)
     % sampling frequency: 1000Hz). Note that the filter induces a transient
     % oscillation of about 400 samples which must be removed from the data
     dennotch = [1 -1.847737249430546 0.987291867964730];
     numnotch = [0.993645933982365 -1.847737249430546 0.993645933982365];
-    Ns = 400;
-elseif (Fs==200)
+elseif (fs==200)
     % sampling frequency: 200Hz). Note that the filter induces a
     % transient oscillation of about 100 samples which must be removed
     % from the data 
     dennotch = [1 0.598862049930572 0.937958302720205];
     numnotch = [0.968979151360102 0.598862049930572 0.968979151360103];
-    Ns = 100;
 else
     error('Error: notch filter not available');
 end
 
 % max number of samples per channel to be extracted
-% 2.5 represents the overlap of the sliding windows
-nsamples = round(2.5*Fs);
+sliding_window_overlap = 2.5;
+nsamples = round(sliding_window_overlap * fs);
 
 % step size of the sliding horizon (in s)
 stepwin = 1;
 
-% frequency bands (in Hz) used during the computation of the connectivity matrices
-% conventional frequencies corresponding to the bands below.
-% EZTrack only uses gamma bands
-bands = [0 4; 4 8; 8 13; 13 30; 30 90];
-
-% labels associated with the frequency bands. Note that the positions in "bands" and "label_bands" match
-label_bands = {'delta';'theta';'alpha';'beta';'gamma'};
-
-% % set the number of bytes used for representing each data sample in the
-% % *.mat file
-% switch format_file
-%     case 'short'
-%         nbytes = 2;
-%     case 'int'
-%         nbytes = 4;
-%     case 'single'
-%         nbytes = 4;
-%     case 'double'
-%         nbytes = 8;
-% end
-
-% set the number of points of the fft
-nfft = Fs;   %2^nextpow2(Fs);
-
 % set the window function for the periodograms
-% TODO: Sabatino chose these parameters in his research.
-% Based on periodigrams and a modified FFT
-convwin = 0.54-0.46*cos((2*pi*(1:nfft))/(nfft-1)); 
+% TODO: Sabatino chose these parameters in his research
+% based on periodigrams and a modified FFT.
+convwin = 0.54 - 0.46 * cos((2*pi*(1:fs)) / (fs-1)); 
 convwin = convwin(:);
 
 % set the range of frequencies for the periodograms (Hz)
-frq = (Fs/2).*linspace(0,1,nfft/2);
+freq_range = (fs/2) .* linspace(0,1,fs/2);
 
 % set the step size for the sub-sections (number of samples)
-stepsize = round(nfft/4);
+stepsize = round(fs/4);
 
 % set the pointer to the last byte before the one to be accessed now
 lastwindow = 0;
 
-% open the log file
-if ~exist(sprintf('%s/adj_log', pathval), 'dir')
-    mkdir(sprintf('%s', pathval), 'adj_log');
-end
-fid0 = fopen(sprintf('%s/adj_log/%s_log.dat',pathval,filename),'w');
 %--------------------------------------------------------------------------
 
 
@@ -184,7 +155,9 @@ fid0 = fopen(sprintf('%s/adj_log/%s_log.dat',pathval,filename),'w');
 % main loop
 %--------------------------------------------------------------------------
 tic
-while (pos< fix((lengthfile - (nsamples-stepwin*Fs))/(stepwin*Fs)))
+limit = fix((file_length - (nsamples-stepwin*fs)) / (stepwin*fs));
+
+while (sample_to_access < limit)
     
     % step 0: check if the current pointer is the offset or not. If it is
     %         the offset then the transient response induced by the notch 
@@ -196,110 +169,65 @@ while (pos< fix((lengthfile - (nsamples-stepwin*Fs))/(stepwin*Fs)))
         tmpwindow = 0;
     end
     
-    
     % step 1: extract the data and apply the notch filter. Note that column
     %         #i in the extracted matrix is filled by data samples from the
     %         recording channel #i.
-    tmpdata = fid.eeg(:, (lastwindow - tmpwindow) + 1:lastwindow + nsamples );
+    tmpdata = eeg_file.eeg(:, (lastwindow - tmpwindow) + 1:lastwindow + nsamples );
     tmpdata = filtfilt(numnotch,dennotch,tmpdata');
     data = tmpdata(tmpwindow + 1:end,:);
-    clear tmpdata tmpbyte
     
-   
     % step 2: pre-process the data. For each channel, subtract the mean
     %         value and divide by the standard deviation. Note that this
     %         step NORMALIZES the data
-    data = data-repmat(mean(data,1),size(data,1),1);
-    data = data./repmat(std(data,[],1),size(data,1),1);
+    data = data - repmat(mean(data,1),size(data,1),1);
+    data = data ./ repmat(std(data,[],1),size(data,1),1);
     data(isnan(data)) = 0;
     
     % step 3: split the data into overlapping sub-sections
-    nsections = ceil((size(data,1)-(nfft-stepsize))/stepsize);
+    nsections = ceil((size(data,1)-(fs-stepsize))/stepsize);
     
     % step 4: compute the cross-power spectra
-    CrossPwr = zeros(nfft/2,num_channels*(num_channels+1)/2);
-%     CrossSpc = zeros(nfft/2,num_channels*(num_channels+1)/2);
+    cross_power = zeros(fs/2, num_channels*(num_channels+1)/2);
     
     for i=1:nsections
-        
         % ... extract the current sub-section
-        datasec = data((i-1)*stepsize+1:min((i-1)*stepsize+nfft,size(data,1)),:);
+        datasec = data((i-1) * stepsize+1:min((i-1) * stepsize+fs, size(data,1)),:);
         
-        % ... avoid computation if the data are all zeros
-        if (sum(sum(abs(datasec)))>0)
-            
-            % ... compute the fft. Note: size(X) = (nfft/2) x #-of-channels
-            X = fft(repmat(convwin(1:min(nfft,size(datasec,1))),1,num_channels).*datasec,nfft); 
-            X = X(1:nfft/2,:);
-
-            % ... set temporary matrices for the computation of the cross-
-            %     power spectra and coherences
-            Y = zeros(nfft/2,num_channels*(num_channels-1)/2);
-            Z = zeros(nfft/2,num_channels*(num_channels-1)/2);
-            pointer = 0;
-            for j=1:num_channels-1
-                Y(:,pointer+1:pointer+num_channels-j) = X(:,j+1:num_channels);
-                Z(:,pointer+1:pointer+num_channels-j) = repmat(X(:,j),1,num_channels-j);
-                pointer = pointer+num_channels-j;
-            end
-
-            % ... compute auto- and cross-spectra
-            CrossPwr = CrossPwr+[X.*conj(X) abs(Z.*conj(Y))];
-%             CrossSpc = CrossSpc+[X Z].*conj([X Y]);
-            clear X Y Z pointer j
+        if (sum(sum(abs(datasec))) == 0)
+            % ... avoid computation if the data are all zeros
+            continue;
         end
-        clear datasec
-    end
-    CrossPwr = CrossPwr./nsections;
-%     CrossSpc = CrossSpc./nsections;
-        
-    % step 5: compute the cross-coherence
-%     CrossChr = ones(nfft/2,num_channels*(num_channels+1)/2);
-%     Y = zeros(nfft/2,num_channels*(num_channels-1)/2);
-%     pointer = 0;
-%     for j=1:num_channels-1
-%         Y(:,pointer+1:pointer+num_channels-j) = CrossSpc(:,j+1:num_channels).*repmat(CrossSpc(:,j),1,num_channels-j);
-%         pointer = pointer+num_channels-j;
-%     end
-%     CrossChr(:,num_channels+1:end) = (CrossSpc(:,num_channels+1:end).*conj(CrossSpc(:,num_channels+1:end)))./Y;
-%     clear Y CrossSpc pointer j nsections data
+            
+        % ... compute the fft. Note: size(X) = (fs/2) x #-of-channels
+        X = fft(repmat(convwin(1:min(fs, size(datasec,1))), 1, num_channels) .* datasec, fs); 
+        X = X(1:fs/2, :);
 
-    % step 6: evaluate the cumulative power and the mean coherence in the
-    %         each frequency band of interest and save in file
-    if ~exist(sprintf('%s/adj_pwr', pathval), 'dir')
-        mkdir(sprintf('%s', pathval), 'adj_pwr');
+        % ... set temporary matrices for the computation of the cross-power spectra
+        Y = zeros(fs/2, num_channels * (num_channels-1)/2);
+        Z = zeros(fs/2, num_channels * (num_channels-1)/2);
+        pointer = 0;
+        for j=1:num_channels-1
+            Y(:,pointer+1:pointer+num_channels-j) = X(:,j+1:num_channels);
+            Z(:,pointer+1:pointer+num_channels-j) = repmat(X(:,j),1,num_channels-j);
+            pointer = pointer+num_channels-j;
+        end
+
+        cross_power = cross_power + [(X .* conj(X)) abs(Z .* conj(Y))];
     end
-    
-%     if ~exist(sprintf('%s/adj_chr', pathval), 'dir')
-%         mkdir(sprintf('%s', pathval), 'adj_chr');
-%     end
-    
-    for k=5%1:size(bands,1)
-        tmp = sum(CrossPwr(frq>=bands(k,1) & frq<bands(k,2),:),1);  tmp = tmp(:)';
-        fid2 = fopen(sprintf('%s/adj_pwr/adj_pwr_%s_%s.dat',pathval,filename,label_bands{k}),'ab');
-        fwrite(fid2,tmp,'single'); 
-        fclose(fid2);
-        clear tmp fid2
-        
-%         tmp = mean(CrossChr(frq>=bands(k,1) & frq<bands(k,2),:),1); tmp = tmp(:)';
-%         fid2 = fopen(sprintf('%s/adj_chr/adj_chr_%s_%s.dat',pathval,filename,label_bands{k}),'ab');
-%         fwrite(fid2,tmp,'double');
-%         fclose(fid2);
-%         clear tmp fid2
-    end
+    cross_power = cross_power ./ nsections;
+
+    % step 6: evaluate the cumulative power in the gamma band and save in file
+    power = sum(cross_power((freq_range >= gamma(1) & freq_range < gamma(2)),:), 1);
+    power = power(:)';
+    fwrite(output_file, power, 'single'); 
 
     % step 7: update the pointer and save the results
-    pos = pos+stepwin;
-    lastwindow = Fs*pos;
-    fprintf(fid0,'window of data #%d completed\n',pos);
-    clear('-regexp','^Cross');
+    sample_to_access = sample_to_access + stepwin;
+    lastwindow = fs * sample_to_access;
+    display(sprintf('completed window #%d of #%d for %s\n', sample_to_access, limit, filename));
 end
 
-
-% save the CPU time required for the computation in the log file and close
-% the log file
-t = toc;
-fprintf(fid0,'cpu time used: %f\n',t);
-fclose(fid0);
+fclose(output_file);
+display(sprintf('power_coherence finished in %fs', toc));
 
 end
