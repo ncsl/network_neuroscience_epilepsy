eef4b9fc2b0067edac523b77514f407bbd020395
Bobby Norton <bobby@testedminds.com>
2016-01-15 15:40:11 -0600

    Start building test patient label and event data on the fly

--- fsv2heatmap/temporal_ieeg_results.m
+++ fsv2heatmap/temporal_ieeg_results.m
@@ -10,8 +10,27 @@ function csv_file = temporal_ieeg_results(test_patient_id)
                          'PY11N003';'PY11N006';'PY12N005';'PY12N008';'PY12N010';...
                          'PY12N012';'PY13N003';'PY13N011';'PY14N004';'PY14N005'};
 
+    % Add test patient info to reference data.
     if (~any(find(ismember(temporal_patients, test_patient_id))))
         temporal_patients{end+1,1} = test_patient_id;
+        
+        % Test patient labels
+        segment_id = '0077';         % TODO: param
+        labels_row = fileread([eztrack_home '/output/eeg/' test_patient_id '/' test_patient_id '_' segment_id '_labels.csv']);
+        labels = strread(labels_row,'%s','delimiter',',')';
+
+        p = struct([]);
+        p(1).labels = struct([]);
+        p(1).labels(1).values = labels;
+        
+        % Test patient events
+        p.events = struct([]);
+        p.events(1).nevents = 1;
+        p.events(1).ttl_electrodes = length(labels);
+        p.events(1).start_marks = 100; % TODO: param - must be greater than 60.
+        p.events(1).end_marks = 230;   % TODO: param - must be at least 60s less than the duration in the file.
+        p.events(1).RR_electrodes = [];
+        patient_info.(test_patient_id) = p;
     end
 
     points = fsv2pcspace(temporal_patients, test_patient_id, patient_info, fsv_path);
