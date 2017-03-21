ad17c210415ed7d8e9e8f45c2693a006071f4486
Adam Li <adam2392@gmail.com>
2017-03-21 14:49:23 -0400

    Adding the updated mat files for jhu and nih.

---
+++ paper/consolidateNIH.m
@@ -0,0 +1,154 @@
+% load('pt1.mat');
+% pt1.E_gauss1 = E_gauss1;
+% pt1.E_gauss2 = E_gauss2;
+% pt1.E_gauss3 = E_gauss3;
+% pt1.E_gauss4 = E_gauss4;
+% pt1.E_labels = E_labels;
+% pt1.E_Weights = E_Weights;
+% 
+% load('pt2.mat');
+% pt2.E_gauss1 = E_gauss1;
+% pt2.E_gauss2 = E_gauss2;
+% pt2.E_gauss3 = E_gauss3;
+% % pt1.E_gauss4 = E_gauss4;
+% pt2.E_labels = E_labels;
+% pt2.E_Weights = E_Weights;
+% 
+% load('pt3.mat');
+% pt3.E_gauss1 = E_gauss1;
+% pt3.E_gauss2 = E_gauss2;
+% pt3.E_gauss3 = E_gauss3;
+% % pt1.E_gauss4 = E_gauss4;
+% pt3.E_labels = E_labels;
+% pt3.E_Weights = E_Weights;
+% 
+% load('pt6.mat');
+% pt6.E_gauss1 = E_gauss1;
+% pt6.E_gauss2 = E_gauss2;
+% pt6.E_gauss3 = E_gauss3;
+% % pt1.E_gauss4 = E_gauss4;
+% pt6.E_labels = E_labels;
+% pt6.E_Weights = E_Weights;
+% 
+% load('pt7.mat');
+% pt7.E_gauss1 = E_gauss1;
+% pt7.E_gauss2 = E_gauss2;
+% pt7.E_gauss3 = E_gauss3;
+% % pt1.E_gauss4 = E_gauss4;
+% pt7.E_labels = E_labels;
+% pt7.E_Weights = E_Weights;
+% 
+% load('pt8.mat');
+% pt8.E_gauss1 = E_gauss1;
+% pt8.E_gauss2 = E_gauss2;
+% pt8.E_gauss3 = E_gauss3;
+% % pt1.E_gauss4 = E_gauss4;
+% pt8.E_labels = E_labels;
+% pt8.E_Weights = E_Weights;
+% 
+% load('pt10.mat');
+% pt10.E_gauss1 = E_gauss1;
+% pt10.E_gauss2 = E_gauss2;
+% pt10.E_gauss3 = E_gauss3;
+% % pt1.E_gauss4 = E_gauss4;
+% pt10.E_labels = E_labels;
+% pt10.E_Weights = E_Weights;
+% 
+% load('pt11.mat');
+% pt11.E_gauss1 = E_gauss1;
+% pt11.E_gauss2 = E_gauss2;
+% pt11.E_gauss3 = E_gauss3;
+% % pt11.E_gauss4 = E_gauss4;
+% pt11.E_labels = E_labels;
+% pt11.E_Weights = E_Weights;
+% 
+% load('pt12.mat');
+% pt12.E_gauss1 = E_gauss1;
+% pt12.E_gauss2 = E_gauss2;
+% % pt7.E_gauss3 = E_gauss3;
+% % pt1.E_gauss4 = E_gauss4;
+% pt12.E_labels = E_labels;
+% pt12.E_Weights = E_Weights;
+% 
+% load('pt13.mat');
+% pt13.E_gauss1 = E_gauss1;
+% pt13.E_gauss2 = E_gauss2;
+% pt13.E_gauss3 = E_gauss3;
+% pt13.E_gauss4 = E_gauss4;
+% pt13.E_gauss5 = E_gauss5;
+% pt13.E_labels = E_labels;
+% pt13.E_Weights = E_Weights;
+% 
+% save('nih_iEEG_results', 'pt1', 'pt2', 'pt3', 'pt6', 'pt7', 'pt8', 'pt10', 'pt11', 'pt12', 'pt13');
+
+
+%% JHU
+load('jhu101.mat');
+jhu101.E_gauss1 = E_gauss1;
+jhu101.E_gauss2 = E_gauss2;
+jhu101.E_gauss3 = E_gauss3;
+jhu101.E_gauss4 = E_gauss4;
+jhu101.E_labels = E_labels;
+jhu101.E_Weights = E_Weights;
+
+load('jhu102.mat');
+jhu102.E_gauss1 = E_gauss1;
+jhu102.E_gauss2 = E_gauss2;
+jhu102.E_gauss3 = E_gauss3;
+jhu102.E_gauss4 = E_gauss4;
+jhu102.E_gauss5 = E_gauss5;
+jhu102.E_gauss6 = E_gauss6;
+jhu102.E_labels = E_labels;
+jhu102.E_Weights = E_Weights;
+
+load('jhu103.mat');
+jhu103.E_gauss1 = E_gauss1;
+jhu103.E_gauss2 = E_gauss2;
+% jhu103.E_gauss3 = E_gauss3;
+% pt1.E_gauss4 = E_gauss4;
+jhu103.E_labels = E_labels;
+jhu103.E_Weights = E_Weights;
+
+load('jhu104.mat');
+jhu104.E_gauss1 = E_gauss1;
+jhu104.E_gauss2 = E_gauss2;
+jhu104.E_gauss3 = E_gauss3;
+% pt1.E_gauss4 = E_gauss4;
+jhu104.E_labels = E_labels;
+jhu104.E_Weights = E_Weights;
+
+load('jhu105.mat');
+jhu105.E_gauss1 = E_gauss1;
+jhu105.E_gauss2 = E_gauss2;
+jhu105.E_gauss3 = E_gauss3;
+jhu105.E_gauss4 = E_gauss4;
+jhu105.E_gauss5 = E_gauss5;
+jhu105.E_labels = E_labels;
+jhu105.E_Weights = E_Weights;
+
+
+load('jhu106.mat');
+jhu106.E_gauss1 = E_gauss1;
+jhu106.E_gauss2 = E_gauss2;
+jhu106.E_gauss3 = E_gauss3;
+jhu106.E_gauss4 = E_gauss4;
+jhu106.E_gauss5 = E_gauss5;
+jhu106.E_gauss6 = E_gauss6;
+jhu106.E_labels = E_labels;
+jhu106.E_Weights = E_Weights;
+
+load('jhu107.mat');
+jhu107.E_gauss1 = E_gauss1;
+jhu107.E_gauss2 = E_gauss2;
+jhu107.E_gauss3 = E_gauss3;
+jhu107.E_gauss4 = E_gauss4;
+jhu107.E_gauss5 = E_gauss5;
+jhu107.E_gauss6 = E_gauss6;
+jhu107.E_gauss7 = E_gauss7;
+jhu107.E_gauss8 = E_gauss8;
+jhu107.E_gauss9 = E_gauss9;
+jhu107.E_labels = E_labels;
+jhu107.E_Weights = E_Weights;
+
+save('jhu_iEEG_results', 'jhu101', 'jhu102', 'jhu103', 'jhu104', 'jhu105', ...
+    'jhu106', 'jhu107');
