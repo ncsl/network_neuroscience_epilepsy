0418d5f6dd993f80abbfa5f912c56f81d5010df2
Adam <adam2392@gmail.com>
2017-03-23 19:48:10 -0400

    Adding eztrack figures.

--- paper/computeOptimalThreshold.m
+++ paper/computeOptimalThreshold.m
@@ -27,13 +27,14 @@ NR_UMMC = {'UMMC001', 'UMMC007', 'UMMC009'};
 SUCCESS = cat(2, SUCCESS_JHUOLD, SUCCESS_JHU, SUCCESS_NIH, SUCCESS_UMMC);
 FAILURE = cat(2, FAILURE_JHU, FAILURE_NIH, FAILURE_OLDJHU);
 NR = cat(2, NR_JHU, NR_UMMC);
+FAILURE_UMMC = NR_UMMC;
 
 SUCCESS = {SUCCESS_JHUOLD, SUCCESS_JHU, SUCCESS_NIH, SUCCESS_UMMC};
-FAILURE = {FAILURE_JHU, FAILURE_NIH, FAILURE_OLDJHU};
+FAILURE = {FAILURE_OLDJHU, FAILURE_JHU, FAILURE_NIH,FAILURE_UMMC};
 NR = {NR_JHU, NR_UMMC};
 %% Set Working Dirs
 homeDir = '/Users/adam2392/Dropbox/EZTrack/PAPER/Data/';
-workDir = '/home/WIN/ali39/Dropbox/EZTrack/Hopkins patients data/results/';
+workDir = '/home/WIN/ali39/Dropbox/EZTrack/PAPER/Data/';
 if ~isempty(dir(homeDir)), rootDir = homeDir;
 elseif ~isempty(dir(workDir)), rootDir = workDir;
 else   error('Neither Work nor Home EEG directories exist! Exiting'); end
@@ -153,7 +154,7 @@ for iCenter=1:length(CENTERS)
         patients_key{end+1} = patient;
         patThresholds(iPat, :) = doas;
     end
-    
+    patients_key
     %% Plotting DOA of SUCCESS For This Center
     figure;
     shadedErrorBar(thresholds, mean(patThresholds,1), std(patThresholds, 0, 1));
@@ -164,94 +165,124 @@ for iCenter=1:length(CENTERS)
     title(titleStr);
     ax.FontSize = FONTSIZE;
     
+    currfig = gcf;
+    set(currfig, 'Units', 'inches');
+    currfig.Position = [17.3438         0   15.9896   11.6771];
+    
+    toSaveFigFile = fullfile(figDir, strcat(CENTERS{iCenter}, '_success_doavsthreshold'));
+    print(toSaveFigFile, '-dpng', '-r0')
 end
 
 
 %% FAILURE
-patients = FAILURE;
-patThresholds = zeros(length(patients), length(thresholds));
-for iPat=1:length(patients)
-    patient = patients{iPat};
+FAILURE = {FAILURE_OLDJHU, FAILURE_JHU, FAILURE_NIH,FAILURE_UMMC};
+CENTERS = {'JHU old', 'JHU', 'NIH', 'UMMC'};
+FILES = {resultsFile, jhuresults, nihresults, ummcresults};
+% hold doa for each patient for each threshold
+thresholds = linspace(0.01,0.95, 100);
+patThresholds = zeros(length([SUCCESS{:}]), length(thresholds)); 
+patients_key = {};
+
+for iCenter=1:length(CENTERS)
+    patients = SUCCESS{iCenter};
+    file = FILES{iCenter};
+    data = load(file);
     
-    % set patientID and seizureID
-    patient_id = patient(1:strfind(patient, 'seiz')-1);
-    seizure_id = strcat('_', patient(strfind(patient, 'seiz'):end));
-    seeg = 1;
-    INTERICTAL = 0;
-    if isempty(patient_id)
-        patient_id = patient(1:strfind(patient, 'sz')-1);
-        seizure_id = patient(strfind(patient, 'sz'):end);
-        seeg = 0;
+    patThresholds = zeros(length(patients), length(thresholds)); 
+    patients_key = {};
+    for iPat=1:length(patients)
+        patient = patients{iPat};
+
+        % set patientID and seizureID
+        patient_id = patient(1:strfind(patient, 'seiz')-1);
+        seizure_id = strcat('_', patient(strfind(patient, 'seiz'):end));
+        seeg = 1;
+        INTERICTAL = 0;
+        if isempty(patient_id)
+            patient_id = patient(1:strfind(patient, 'sz')-1);
+            seizure_id = patient(strfind(patient, 'sz'):end);
+            seeg = 0;
+        end
+        if isempty(patient_id)
+            patient_id = patient(1:strfind(patient, 'aslp')-1);
+            seizure_id = patient(strfind(patient, 'aslp'):end);
+            seeg = 0;
+            INTERICTAL = 1;
+        end
+        if isempty(patient_id)
+            patient_id = patient(1:strfind(patient, 'aw')-1);
+            seizure_id = patient(strfind(patient, 'aw'):end);
+            seeg = 0;
+            INTERICTAL = 1;
+        end
+
+        try
+            patData = data.(patient);
+        catch e
+            disp(e);
+            patData = data.(lower(patient));
+        end
+        % get the weights and the labels
+        e_weights = patData.E_Weights;
+%         outcome = patData.Outcome;
+    
+        % ALL
+        elec_labels = patData.E_labels;
+
+        % CEZ
+        try
+            resect_labels = patData.R_E_labels(~cellfun('isempty', patData.R_E_labels));
+        catch e
+            resect_labels = patData.onset_electrodes;
+        end
+        % based on a certain threshold, compute EEZ for this dataset
+        thresholds = linspace(0.1, 0.95, 100); % create range of thresholds to test
+        doas = zeros(length(thresholds), 1);
+        % for each threshold, compute doa using jaccard index
+        for iThresh=1:length(thresholds)
+            threshold = thresholds(iThresh);
+
+            % compute EEZ
+            EEZ = elec_labels(find(e_weights > threshold));
+
+            % compute Jaccard Index
+            try
+                doa = DOA(EEZ, resect_labels, elec_labels, 'jaccard');
+            catch e
+                disp(e)
+            end
+
+            doas(iThresh) = doa;
+        end
+
+        % compute optimal threshold
+        max_index = find(doas == max(doas));
+    
+        % save optimal thresholds
+        if length(max_index) > 1
+            max_index = max_index(1);
+        end
+        patients_key{end+1} = patient;
+        patThresholds(iPat, :) = doas;
     end
-    if isempty(patient_id)
-        patient_id = patient(1:strfind(patient, 'aslp')-1);
-        seizure_id = patient(strfind(patient, 'aslp'):end);
-        seeg = 0;
-        INTERICTAL = 1;
-    end
-    if isempty(patient_id)
-        patient_id = patient(1:strfind(patient, 'aw')-1);
-        seizure_id = patient(strfind(patient, 'aw'):end);
-        seeg = 0;
-        INTERICTAL = 1;
-    end
     
-    patData = data.(patient);
-    
-    % get the weights and the labels
-    e_weights = patData.E_Weights;
-    outcome = patData.Outcome;
-    
-    % ALL
-    elec_labels = patData.E_labels;
-    
-    % CEZ
-    resect_labels = patData.R_E_labels(~cellfun('isempty', patData.R_E_labels));
-    
-    % based on a certain threshold, compute EEZ for this dataset
-    thresholds = linspace(0.1, 0.95, 100); % create range of thresholds to test
-    doas = zeros(length(thresholds), 1);
-    % for each threshold, compute doa using jaccard index
-    for iThresh=1:length(thresholds)
-        threshold = thresholds(iThresh);
-        
-        % compute EEZ
-        EEZ = elec_labels(find(e_weights > threshold));
-        
-        % compute Jaccard Index
-        doa = DOA(EEZ, resect_labels, elec_labels, 'jaccard');
-        
-        doas(iThresh) = doa;
-    end
-    
-    % compute optimal threshold
-    max_index = find(doas == max(doas));
-    
-    % plotting doa vs threshold for this patient
+    %% Plotting DOA of SUCCESS For This Center
     figure;
-    titleStr = {[patient, ' EZTrack | ', outcome{:}], ['Degree of Agreement']};
-    plot(thresholds, doas, 'k-'); hold on;
-    plot(thresholds(max_index), doas(max_index), 'r*');
-    axes = gca;
-    XLIM = axes.XLim;
-    YLIM = axes.YLim;
-    axes.FontSize = FONTSIZE;
-    xlabel('Thresholds');
-    ylabel('Jaccard Index');
+    shadedErrorBar(thresholds, mean(patThresholds,1), std(patThresholds, 0, 1));
+    ax = gca;
+    xlabel('Thresholds on Degree of Agreement');
+    ylabel('Degree of Agreement (Jaccard Index)');
+    titleStr = {[CENTERS{iCenter}, ' FAilure Patients']};
     title(titleStr);
-    text(mean(XLIM), mean(YLIM), ['Max DOA at ', num2str(thresholds(max_index))]);
+    ax.FontSize = FONTSIZE;
     
-    % save optimal thresholds
-    if length(max_index) > 1
-        max_index = max_index(1);
-    end
-    patThresholds(iPat, :) = doas;
+    currfig = gcf;
+    set(currfig, 'Units', 'inches');
+    currfig.Position = [17.3438         0   15.9896   11.6771];
+    
+    toSaveFigFile = fullfile(figDir, strcat(CENTERS{iCenter}, '_failure_doavsthreshold'));
+    print(toSaveFigFile, '-dpng', '-r0')
 end
-
-figure;
-shadedErrorBar(thresholds, mean(patThresholds,1), std(patThresholds, 0, 1));
-
-
 
 %% Do All Patients
 % patThresholds = zeros(length(patients), length(thresholds));
